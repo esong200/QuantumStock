14eeafa88f923dc54eeb3a6a5b644a25a00a2e8d
whoisis <readatontim@gmail.com>
2018-08-02 16:19:01 -0700

    Test Commit

--- Neural Network Test/src/functions.java
+++ Neural Network Test/src/functions.java
@@ -14,9 +14,9 @@ public class functions {
 	}
 	public static double[][] dotMultiply(double[][] a, double[][] b){
 		double[][]returnArr = new double[a.length][b[0].length];
-		for (int i = 0; i < a.length; i++) { 
-		    for (int j = 0; j < b[0].length; j++) { 
-		        for (int k = 0; k < a[0].length; k++) { 
+		for (int i = 0; i < a.length; i++) {
+		    for (int j = 0; j < b[0].length; j++) {
+		        for (int k = 0; k < a[0].length; k++) {
 		            returnArr[i][j] += a[i][k] * b[k][j];
 		        }
 		    }
@@ -29,7 +29,7 @@ public class functions {
 		}else
 			return 1/(1+Math.pow(Math.E,(-x))) * (1-(1/(1+Math.pow(Math.E,(-x)))));
 	}
-	
+
 	public static double[] sigmoid(double[]x, boolean deriv) {
 		double[] Return = new double[x.length];
 		for(int i = 0; i< x.length; i++) {
@@ -37,7 +37,7 @@ public class functions {
 		}
 		return Return;
 	}
-	
+
 	public static double[][] sigmoid(double[][]x, boolean deriv){
 		double[][] Return = new double[x.length][x[0].length];
 		for(int i = 0; i<x.length; i++) {
@@ -45,7 +45,7 @@ public class functions {
 		}
 		return Return;
 	}
-	
+
 	public static double[] rotateMultiply(double[][]b, double[]a){
 		double[]returnArr = new double[a.length];
 		double [][] rotatedArr = new double[b[0].length][b.length];
@@ -55,7 +55,7 @@ public class functions {
 			}
 		}
 		returnArr = dotMultiply(a,rotatedArr);
-		
+
 		return returnArr;
 	}
 	public static double[][] rotateMultiply(double[]noRotate, double[]rotate){
@@ -65,7 +65,7 @@ public class functions {
 				returnArr[j][i]=noRotate[i]*rotate[j];
 			}
 		}
-		return returnArr;	
+		return returnArr;
 	}
 	public static double[][] rotateMultiply(double[][]a, double[][]b){
 		double[][] returnArr = new double[a.length][b.length];
