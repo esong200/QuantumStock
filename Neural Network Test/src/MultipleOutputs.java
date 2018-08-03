deba2e666c05d8a498d7900a571413222feafbd6
whoisis <readatontim@gmail.com>
2018-08-02 20:21:52 -0700

    adds beating sp 500

--- Neural Network Test/src/MultipleOutputs.java
+++ Neural Network Test/src/MultipleOutputs.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
 
 public class MultipleOutputs extends functions {
-//Outputs in format {<-10%, -10% - -5%, -5% - -2%, -2% - 2%, 2% - 5%, 5% - 10%, >10%}
+//Outputs in format {<-10%, -10% - -5%, -5% - -2%, -2% - 2%, 2% - 5%, 5% - 10%, >10%, beat S&P 500}
  public static void main(String[] args) {
  	String rawData="";
  	double[][] inputs = new double[35][55];
  	String[][] inputStrings  = new String[35][55];
  	double[][]synapticWeights0 = new double[55][35] ;
  	double[][]intermediateAnswer = new double[35][35];
- 	double[][] synapticWeights1 = new double[35][7] ;
- 	double[][]finalAnsArr = new double[35][7];
- 	double[][] desiredOutcome = new double[35][7]; //need more data to put the desired Outcome
+ 	double[][] synapticWeights1 = new double[35][8] ;
+ 	double[][]finalAnsArr = new double[35][8];
+ 	double[][] desiredOutcome = new double[35][8]; //need more data to put the desired Outcome
 
   //to read inputs can be replaced w/ excel reading
 	System.out.print("Enter data: ");
