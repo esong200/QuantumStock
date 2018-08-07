424c3a1274a6bc3f1e065d766fb858b17a7cc9d1
whoisis <readatontim@gmail.com>
2018-08-07 01:39:18 -0700

    uhhh no idea what happened but need to push?

--- Neural Network Test/src/MultipleOutputs.java
+++ Neural Network Test/src/MultipleOutputs.java
@@ -1,3 +1,4 @@
+<<<<<<< current
 import java.util.Scanner;
 
 public class MultipleOutputs extends functions, DataReader {
@@ -86,3 +87,88 @@ public class MultipleOutputs extends functions, DataReader {
  }
 
  }
+=======
+import java.util.Scanner;
+
+public class MultipleOutputs extends functions {
+//Outputs in format {<-10%, -10% - -5%, -5% - -2%, -2% - 2%, 2% - 5%, 5% - 10%, >10%}
+ public static void main(String[] args) {
+ 	String rawData="";
+ 	double[][] inputs = new double[35][55];
+ 	String[][] inputStrings  = new String[35][55];
+ 	double[][]synapticWeights0 = new double[55][35] ;
+ 	double[][]intermediateAnswer = new double[35][35];
+ 	double[][] synapticWeights1 = new double[35][7] ;
+ 	double[][]finalAnsArr = new double[35][7];
+ 	double[][] desiredOutcome = new double[35][7]; //need more data to put the desired Outcome
+
+  //to read inputs can be replaced w/ excel reading
+	System.out.print("Enter data: ");
+ 	Scanner scan = new Scanner (System.in);
+ 	if(scan.hasNext()) {
+ 		for(int i=0; i<55; i++) {
+ 			rawData=scan.nextLine();
+ 			rawData=rawData.replaceAll(" ","");
+ 			for(int j=0; j<35; j++) {
+ 				inputs[j][i]=Double.parseDouble(rawData.substring(0,rawData.indexOf("%")));
+ 				rawData = rawData.replaceFirst(rawData.substring(0,rawData.indexOf("%")+1),"");
+ 			}
+ 		}
+ 	}
+
+
+ //synaptic training
+ 	for(int m = 0; m<1000; m++) {
+ 		intermediateAnswer = sigmoid(dotMultiply(inputs, synapticWeights0), false);
+ 		finalAnsArr = sigmoid(dotMultiply(synapticWeights1, intermediateAnswer), false);
+ 		double[][] error1 = new double[desiredOutcome.length][desiredOutcome[0]];
+ 		double[][] delta1 = new double[desiredOutcome.length][desiredOutcome[0]];
+ 		System.out.println("Training Answer "+m+":");
+ 		for(int i=0; i<delta1.length; i++) {
+			for(int j = 0; j<delta1[0].length; j++){
+ 			//System.out.println(finalAnsArr[i]);
+ 				error1[i][j]=desiredOutcome[i][j]-finalAnsArr[i][j];
+ 				delta1[i][j]= error1[i][j]*sigmoid(finalAnsArr[i][j],true);
+ 		}
+	}
+ 		double[][] error0 =  rotateMultiply(synapticWeights1,delta1);
+ 		double[][] delta0 = new double[error0.length][error0[0].length]; /*rotateMultiply(synapticWeights1,delta1);*/
+
+ 		for(int i = 0; i<delta0.length; i++) {
+ 			for(int j=0; j<delta0[0].length; j++) {
+ 				delta0[i][j]=error0[i][j] * sigmoid(intermediateAnswer,true)[i][j];
+ 			}
+ 		}
+ 		for(int i=0; i<delta1.length; i++) {
+ 			double[] x = rotateMultiply(intermediateAnswer, delta1);
+ 			synapticWeights1[i]+= x[i];
+ 		}
+
+ 		for (int i = 0; i<synapticWeights0.length; i++) {
+ 			for(int j = 0; j<synapticWeights0[0].length; j++) {
+ 				synapticWeights0[i][j] += rotateMultiply(delta0, inputs )[i][j];
+ 			}
+ 		}
+ 		System.out.println();
+ 	}
+ 	System.out.println();
+ 	System.out.println("Synaptic Weights1:");
+ 	for(double i: synapticWeights1) {
+ 		System.out.println(i);
+ 		System.out.println();
+ 	}
+ 	System.out.println();
+ 	System.out.println("Synaptic Weights0:");
+ 	for(double i[]: synapticWeights0) {
+ 		System.out.println();
+ 		for(double x: i) {
+ 			System.out.print(x + "\t");
+ 		}
+ 		System.out.println();
+
+ 	}
+ 	scan.close();
+ }
+
+ }
+>>>>>>> before discard
--- Neural Network Test/src/firstQuarterlyTests.java
+++ Neural Network Test/src/firstQuarterlyTests.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class largeArray extends functions {
+public class firstQuarterlyTests extends functions {
 
 	public static void main(String[] args) {
 		String rawData="";
@@ -8,16 +8,9 @@ public class largeArray extends functions {
 		String[][] inputStrings  = new String[35][55];
 		double[][]synapticWeights0 = new double[55][35] ;
 		double[][]intermediateAnswer = new double[35][35];
-		double[] synapticWeights1 = new double[35];
+		double[] synapticWeights1 = new double[35] ;
 		double[]finalAnsArr = new double[35];
 		double[] desiredOutcome = {0,0,1,1,1,1,0,1,1,0,1,0,1,0,0,1,1,1,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,1,1};
-		double[] error1 = new double[desiredOutcome.length];
-		double[] delta1 = new double[desiredOutcome.length];
-		double[][] delta0 = new double[35][35];
-		int numOfIterations = 50;
-		double[] times = new double[numOfIterations];
-
-
 		System.out.print("Enter data: ");
 		Scanner scan = new Scanner (System.in);
 		if(scan.hasNext()) {
@@ -33,10 +26,12 @@ public class largeArray extends functions {
 		for (int i=0; i<synapticWeights1.length; i++) {
 			synapticWeights1[i] = (2*Math.random()) -1;
 		}
-		for(int m = 0; m<numOfIterations; m++) {
-			long start = System.currentTimeMillis();
-			intermediateAnswer = sigmoid(dotMultiplyFastest(inputs, synapticWeights0), false);
+
+		for(int m = 0; m<10; m++) {
+			intermediateAnswer = sigmoid(dotMultiply(inputs, synapticWeights0), false);
 			finalAnsArr = sigmoid(dotMultiply(synapticWeights1, intermediateAnswer), false);
+			double[] error1 = new double[desiredOutcome.length];
+			double[] delta1 = new double[desiredOutcome.length];
 			System.out.println("Training Answer "+m+":");
 			for(int i=0; i<delta1.length; i++) {
 				System.out.println(finalAnsArr[i]);
@@ -44,7 +39,7 @@ public class largeArray extends functions {
 				delta1[i]= error1[i]*sigmoid(finalAnsArr[i],true);
 			}
 			double[][] error0 =  rotateMultiply(synapticWeights1,delta1);
-            //rotateMultiply(synapticWeights1,delta1);
+			double[][] delta0 = new double[35][35]; /*rotateMultiply(synapticWeights1,delta1);*/
 
 			for(int i = 0; i<delta0.length; i++) {
 				for(int j=0; j<delta0[0].length; j++) {
@@ -52,7 +47,8 @@ public class largeArray extends functions {
 				}
 			}
 			for(int i=0; i<delta1.length; i++) {
-				synapticWeights1[i] += rotateMultiply(intermediateAnswer, delta1)[i];
+				double[] x = rotateMultiply(intermediateAnswer, delta1);
+				synapticWeights1[i]+= x[i];
 			}
 
 			for (int i = 0; i<synapticWeights0.length; i++) {
@@ -60,20 +56,10 @@ public class largeArray extends functions {
 					synapticWeights0[i][j] += rotateMultiply(delta0, inputs )[i][j];
 				}
 			}
-			long stop = System.currentTimeMillis();
-			double elapsed = (stop - start) / 1000.0;
-			times[m]=elapsed;
-			//System.out.println(m);
-			System.out.println("Time:" + elapsed);
-
+			System.out.println();
 		}
 		System.out.println();
-		double total = 0;
-		for(int i=0; i<times.length; i++) {
-			total += times[i];
-		}
-		System.out.println("Average time per iteration: "+ (total/times.length));
-		/*System.out.println("Synaptic Weights1:");
+		System.out.println("Synaptic Weights1:");
 		for(double i: synapticWeights1) {
 			System.out.println(i);
 			System.out.println();
@@ -88,9 +74,7 @@ public class largeArray extends functions {
 			System.out.println();
 
 		}
-		*/
 		scan.close();
-
 	}
 
 }
