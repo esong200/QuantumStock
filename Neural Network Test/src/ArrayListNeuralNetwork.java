f34e8b8da3308dd492d86d1984812974a7097d2c
esong200 <ethansong@Ethans-MacBook-Pro-2.local>
2018-08-08 18:27:03 -0700

    Updated Neural Network

--- Neural Network Test/src/ArrayListNeuralNetwork.java
+++ Neural Network Test/src/ArrayListNeuralNetwork.java
@@ -2,81 +2,93 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class ArrayListNeuralNetwork extends functions {
+	//array specs: input array: 1x29, desiredOutcome: 1x8
 	public static void main(String[] args) {
-		String rawData="";
+		ArrayList<double[]> data = DataCollection("AAPL");
+		ArrayList<double[]> ans = answerCompile("AAPL");
 
-		ArrayList<double[]> data = DataCollection("MSFT");
-		ArrayList<double[]> ans = answerCompile("MSFT");
 		int maxSize = data.size();
 		while(ans.size()>maxSize){
 			ans.remove(ans.size()-1);
-		}//make arrays the same size
-		for(int i=0; i<data.size(); i++) {
-		System.out.println();
-		for(int j=0; j<data.get(i).length; j++) {
-			System.out.print(data.get(i)[j] + "\t");
-			inputs[i][j] = data.get(i)[j];
 		}
-	}
-	for(int i=0; i<ans.size(); i++) {
-		System.out.println();
-		for(int j=0; j<ans.get(i).length; j++) {
-			System.out.print(ans.get(i)[j] + "\t");
-			desiredOutcome[i][j] = ans.get(i)[j];
+		double[] inputs = new double[data.get(0).length];
+		double[] desiredOutcome = new double[ans.get(0).length];
+		for(int i=0; i<data.get(0).length; i++) {
+			inputs[i]=data.get(0)[i];
 		}
-	}
-
-
-		/*System.out.print("Enter data: ");
-		Scanner scan = new Scanner (System.in);
-		if(scan.hasNext()) {
-			for(int i=0; i<55; i++) {
-				rawData=scan.nextLine();
-				rawData=rawData.replaceAll(" ","");
-				for(int j=0; j<35; j++) {
-					inputs[j][i]=Double.parseDouble(rawData.substring(0,rawData.indexOf("%")));
-					rawData = rawData.replaceFirst(rawData.substring(0,rawData.indexOf("%")+1),"");
-				}
-			}
+		for(int i=0; i<ans.get(0).length; i++) {
+			desiredOutcome[i] = ans.get(0)[i];
 		}
-		*/
-
+		//double[] inputs = new double[];
+		double[][]synapticWeights0 = new double[inputs.length][18];
+		double[]intermediateAnswer = new double[synapticWeights0[0].length];
+		double[][] synapticWeights1 = new double[intermediateAnswer.length][desiredOutcome.length];
+		double[]finalAnsArr = new double[desiredOutcome.length];
+		double[] error1 = new double[desiredOutcome.length];
+		double[] delta1 = new double[desiredOutcome.length];
+		double[] error0 = new double[intermediateAnswer.length];
+		double[] delta0 = new double[intermediateAnswer.length];
+		int numOfOuterIterations = 1000;
+		double[] times = new double[numOfOuterIterations];
 			for (int i=0; i<synapticWeights1.length; i++) {
-				synapticWeights1[i] = (2*Math.random()) -1;
+				for(int j=0; j<synapticWeights1[0].length; j++) {
+					synapticWeights1[i][j] = (2*Math.random()) -1;
+				}
 			}
-		for(int m = 0; m<numOfIterations; m++) {
+		double[] testArr1 = new double[2];
+		double[][] testArr2 = new double[2][3];
+		for(int i=0; i<testArr1.length; i++) {
+			testArr1[i] = (2*Math.random()) -1;
+		}
+		for(int i=0; i<testArr2.length; i++) {
+			for(int j=0; j<testArr2[0].length; j++) {
+				testArr2[i][j] = (2*Math.random()) -1;
+			}
+		}
+		double[] ansArr = new double[3];
+		ansArr = dotMultiply(testArr1, testArr2);
+		for(int k=0; k<data.size(); k++) {
 			long start = System.currentTimeMillis();
-			intermediateAnswer = sigmoid(dotMultiplyFastest(inputs, synapticWeights0), false);
-			finalAnsArr = sigmoid(dotMultiply(synapticWeights1, intermediateAnswer), false);
-			System.out.println("Training Answer "+m+":");
-			for(int i=0; i<delta1.length; i++) {
-				System.out.println(finalAnsArr[i]);
-				error1[i]=desiredOutcome[i]-finalAnsArr[i];
-				delta1[i]= error1[i]*sigmoid(finalAnsArr[i],true);
-			}
-			double[][] error0 =  rotateMultiply(synapticWeights1,delta1);
-						//rotateMultiply(synapticWeights1,delta1);
+			for(int m = 0; m<50; m++) {
+				intermediateAnswer/*1x18*/ = sigmoid1d(dotMultiply(inputs/*1x29*/, synapticWeights0/*29x18*/), false);
+				finalAnsArr = sigmoid1d(dotMultiply(synapticWeights1, intermediateAnswer), false);
 
-			for(int i = 0; i<delta0.length; i++) {
-				for(int j=0; j<delta0[0].length; j++) {
-					delta0[i][j]=error0[i][j] * sigmoid(intermediateAnswer,true)[i][j];
+				System.out.println("Training Answer "+m+":");
+				for(int i=0; i<delta1.length; i++) {
+					System.out.println(finalAnsArr[i]);
+					error1[i]=desiredOutcome[i]-finalAnsArr[i];
+					delta1[i]= error1[i]*sigmoid(finalAnsArr[i],true);
+				}
+				error0 = rotateMultiply(synapticWeights1,delta1);
+				for(int i = 0; i<delta0.length; i++) {
+					delta0[i]= error0[i] * (sigmoid1d(intermediateAnswer,true)[i]);
+				}
+				for(int i=0; i<synapticWeights1.length; i++) {
+					for(int j=0; j<synapticWeights1[0].length; j++) {
+						synapticWeights1[i][j] +=  rotateMultiply(delta1 , intermediateAnswer)[i][j];
+					}
+
+				}
+
+				for (int i = 0; i<synapticWeights0.length; i++) {
+					for(int j = 0; j<synapticWeights0[0].length; j++) {
+						double[][] debugger = rotateMultiply(delta0, inputs);
+						synapticWeights0[i][j] += debugger[i][j];
+					}
 				}
 			}
-			for(int i=0; i<delta1.length; i++) {
-				synapticWeights1[i] += rotateMultiply(intermediateAnswer, delta1)[i];
+			for(int i=0; i<data.get(k).length; i++) {
+				inputs[i]=data.get(k)[i];
 			}
-
-			for (int i = 0; i<synapticWeights0.length; i++) {
-				for(int j = 0; j<synapticWeights0[0].length; j++) {
-					synapticWeights0[i][j] += rotateMultiply(delta0, inputs )[i][j];
-				}
+			for(int i=0; i<ans.get(k).length; i++) {
+				desiredOutcome[i] = ans.get(k)[i];
 			}
 			long stop = System.currentTimeMillis();
 			double elapsed = (stop - start) / 1000.0;
-			times[m]=elapsed;
+			times[k]=elapsed;
 			//System.out.println(m);
 			System.out.println("Time:" + elapsed);
-
+			//inputs
 		}
 		System.out.println();
 		double total = 0;
@@ -84,6 +96,7 @@ public class ArrayListNeuralNetwork extends functions {
 			total += times[i];
 		}
 		System.out.println("Average time per iteration: "+ (total/times.length));
+
 		/*System.out.println("Synaptic Weights1:");
 		for(double i: synapticWeights1) {
 			System.out.println(i);
