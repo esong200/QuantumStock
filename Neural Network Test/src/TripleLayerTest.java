6fab8eb2c4f7baffedfde8603311b550ff9d23e2
esong200 <ethansong@Ethans-MacBook-Pro-2.local>
2018-08-09 17:26:26 -0700

    more triple layer

--- Neural Network Test/src/TripleLayerTest.java
+++ Neural Network Test/src/TripleLayerTest.java
@@ -24,6 +24,37 @@ public class TripleLayerTest extends functions{
 			intermediateAnswer1 = sigmoid1d(dotMultiply(intermediateAnswer0, synapticWeights1), false);
 			finalAnsArr = sigmoid1d(dotMultiply(synapticWeights1, intermediateAnswer1), false);
 			System.out.println("Training Answers "+m+":");
+      for(int i=0; i<delta2.length; i++) {
+				System.out.println(finalAnsArr[i]);
+				error2[i]=desiredOutcome[i]-finalAnsArr[i];
+				delta2[i]= error2[i]*sigmoid(finalAnsArr[i],true);
+			}
+			error1 = rotateMultiply(synapticWeights2,delta2);
+			for(int i = 0; i<delta1.length; i++) {
+				delta1[i]= error1[i] * (sigmoid1d(intermediateAnswer1,true)[i]);
+			}
+			error0 = rotateMultiply(synapticWeights1,delta1);
+			for(int i = 0; i<delta0.length; i++) {
+				delta0[i]= error0[i] * (sigmoid1d(intermediateAnswer0,true)[i]);
+			}
+			for(int i=0; i<synapticWeights2.length; i++) {
+				for(int j=0; j<synapticWeights2[0].length; j++) {
+					synapticWeights2[i][j] +=  rotateMultiply(delta2 , intermediateAnswer1)[i][j];
+				}
+			}
+			for (int i = 0; i<synapticWeights1.length; i++) {
+				for(int j = 0; j<synapticWeights1[0].length; j++) {
+					double[][] debugger = rotateMultiply(delta1, intermediateAnswer0);
+					synapticWeights1[i][j] += debugger[i][j];
+				}
+			}
+			for(int i=0; i<synapticWeights0.length; i++) {
+				for(int j=0; j<synapticWeights0[0].length; j++) {
+					synapticWeights0[i][j] +=  rotateMultiply(delta0 , inputs)[i][j];
+				}
+			}
+			System.out.println();
+		}
 		}
 
 	}
