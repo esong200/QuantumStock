c87d2fb3b2b00b305a08b81fdf2b946b38be0bc0
whoisis <readatontim@gmail.com>
2018-08-10 18:05:45 -0700

    redo gdi

--- Neural Network Test/src/TripleLayerTest.java
+++ Neural Network Test/src/TripleLayerTest.java
@@ -1,12 +1,11 @@
-import java.util.Scanner;
 import java.util.ArrayList;
 
-public class asdfasdfdfd extends functions {
-	//array specs: input array: 1x29, desiredOutcome: 1x8
+public class TripleLayerTest extends functions{
 	public static void main(String[] args) {
-		ArrayList<double[]> data = readCsv("C:\\Users\\Tim Huang\\Documents\\GitHub\\highlighter\\Neural Network Test\\Data\\AllDataAdjst.csv");
-		ArrayList<double[]> ans = readCsv("C:\\Users\\Tim Huang\\Documents\\GitHub\\highlighter\\Neural Network Test\\Data\\AllAns.csv");
+		ArrayList<double[]> data = readCsv("C:\\Users\\Tim Huang\\Documents\\GitHub\\highlighter\\Neural Network Test\\Data\\PEPDataAdjst.csv");
+		ArrayList<double[]> ans = readCsv("C:\\Users\\Tim Huang\\Documents\\GitHub\\highlighter\\Neural Network Test\\Data\\PEPAns.csv");
 		ArrayList<double[]> dataTaylored = data;
+		ArrayList<Integer> correct = new ArrayList<Integer>();
 		double[][] dataTayloredMatrix = new double[dataTaylored.size()][dataTaylored.get(0).length];
 		int maxSize = data.size();
 		while(ans.size()>maxSize){
@@ -24,119 +23,150 @@ public class asdfasdfdfd extends functions {
 		for(int i=0; i<data.get(0).length; i++) {
 			inputs[i]=(dataTaylored.get(0)[i]);
 		}
-		//writeCsv(dataTayloredMatrix, "/Users/ethansong/Documents/Stock Data/stockData10xBigger.csv");
-		//writeCsv(inputs, "/Users/ethansong/Documents/Stock Data/10xANDVDataAdjst.csv");
 
-		//double[] inputs = new double[];
-		double[][]synapticWeights0 = new double[inputs.length][18];
-		double[]intermediateAnswer = new double[synapticWeights0[0].length];
-		double[][] synapticWeights1 = new double[intermediateAnswer.length][desiredOutcome.length];
+		double[][]synapticWeights0 = new double[inputs.length][23];
+		double[]	intermediateAnswer0 = new double[synapticWeights0[0].length];
+		double[][] synapticWeights1 = new double[intermediateAnswer0.length][18];
+		double[]intermediateAnswer1 = new double[synapticWeights1[0].length];
+		double[][] synapticWeights2 = new double[intermediateAnswer1.length][desiredOutcome.length];
 		double[]finalAnsArr = new double[desiredOutcome.length];
-		double[] error1 = new double[desiredOutcome.length];
-		double[] delta1 = new double[desiredOutcome.length];
-		double[] error0 = new double[intermediateAnswer.length];
-		double[] delta0 = new double[intermediateAnswer.length];
-		int numOfOuterIterations = 1000;
-		double[] times = new double[data.size()];
-			for (int i=0; i<synapticWeights1.length; i++) {
-				for(int j=0; j<synapticWeights1[0].length; j++) {
-					synapticWeights1[i][j] = Math.random() - 0.5;
-				}
+		double[] error2 = new double[desiredOutcome.length];
+		double[] delta2 = new double[desiredOutcome.length];
+		double[] error1 = new double[intermediateAnswer1.length];
+		double[] delta1 = new double[intermediateAnswer1.length];
+		double[] error0 = new double[intermediateAnswer0.length];
+		double[] delta0 = new double[intermediateAnswer0.length];
+		double totalTime = 0;
+		double elapsed = 0;
+
+		for (int i=0; i<synapticWeights0.length; i++) {
+			for(int j=0; j<synapticWeights0[0].length; j++) {
+				synapticWeights0[i][j] = (2*Math.random()) -1;
 			}
-		long outermostStart = System.currentTimeMillis();
-		for(int k=0; k<1; k++) {
+		}
+		for (int i=0; i<synapticWeights1.length; i++) {
+			for(int j=0; j<synapticWeights1[0].length; j++) {
+				synapticWeights1[i][j] = (2*Math.random()) -1;
+			}
+		}
+		for (int i=0; i<synapticWeights2.length; i++) {
+			for(int j=0; j<synapticWeights2[0].length; j++) {
+				synapticWeights2[i][j] = (2*Math.random()) -1;
+			}
+		}
+
+
+		for(int m = 0; m<1000000; m++) {
+			int rand = (int) ((int) dataTaylored.size()*Math.random());
+			for(int i=0; i<data.get(0).length; i++) {
+				inputs[i]=(dataTaylored.get(rand))[i];
+			}
+			for(int i=0; i<ans.get(0).length; i++) {
+				desiredOutcome[i] = ans.get(rand)[i];
+			}
 			long start = System.currentTimeMillis();
-			for(int m = 0; m<100000000; m++) {
-				int rand = (int) ((int) dataTaylored.size()*Math.random());
-				//System.out.println(rand);
-				for(int i=0; i<data.get(0).length; i++) {
-					inputs[i]=(dataTaylored.get(rand))[i];
-				}
-				for(int i=0; i<ans.get(0).length; i++) {
-					desiredOutcome[i] = ans.get(rand)[i];
-				}
+			intermediateAnswer0 = sigmoid1d(dotMultiply(inputs/*1x29*/, synapticWeights0/*29x18*/), false);
 
-				intermediateAnswer/*1x18*/ = sigmoid1d(dotMultiply(inputs/*1x29*/, synapticWeights0/*29x18*/), false);
-				int countIA = 0;
-				for(double i: dotMultiply(inputs, synapticWeights0)) {
-		//			System.out.println("Dot Multiplied IA results"+countIA);
-		//			System.out.println(i);
-					countIA++;
-				}
-				int countFA = 0;
-				finalAnsArr = sigmoid1d(dotMultiply(intermediateAnswer, synapticWeights1), false);
-				//System.out.println("Training Answers "+m+" inside outer iteration "+k+":");
-				for(int i=0; i<delta1.length; i++) {
-				//	System.out.println(finalAnsArr[i]);
-					error1[i]=desiredOutcome[i]-finalAnsArr[i];
-					delta1[i]= error1[i]*sigmoid(finalAnsArr[i],true);
-				}
-				error0 = rotateMultiply(synapticWeights1,delta1);
-				for(int i = 0; i<delta0.length; i++) {
-					delta0[i]= error0[i] * (sigmoid1d(intermediateAnswer,true)[i]);
-				}
-				for(int i=0; i<synapticWeights1.length; i++) {
-					for(int j=0; j<synapticWeights1[0].length; j++) {
-						synapticWeights1[i][j] +=  rotateMultiply(delta1 , intermediateAnswer)[i][j];
-					}
+			intermediateAnswer1/*1x18*/ = sigmoid1d(dotMultiply(intermediateAnswer0/*1x29*/, synapticWeights1/*29x18*/), false);
+			finalAnsArr = sigmoid1d(dotMultiply(intermediateAnswer1, synapticWeights2), false);
+			//System.out.println();
+			//System.out.println("Training Answers "+m+" inside outer iteration :");
+			for(int i=0; i<delta2.length; i++) {
+				//System.out.println(finalAnsArr[i]);
+				error2[i]=desiredOutcome[i]-finalAnsArr[i];
+				delta2[i]= error2[i]*sigmoid(finalAnsArr[i],true);
+			}
 
-				}
-
-				for (int i = 0; i<synapticWeights0.length; i++) {
-					for(int j = 0; j<synapticWeights0[0].length; j++) {
-						double[][] debugger = rotateMultiply(delta0, inputs);
-						synapticWeights0[i][j] += debugger[i][j];
-					}
-				}
-				if(m%1000 == 0) {
-					double  avg = 0;
-					for(double i: error1) {
-						avg+=i;
-					}
-					avg= avg/error1.length;
-					System.out.println("error " + m + " :" + avg);
+			error1 = rotateMultiply(synapticWeights2,delta2);
+			for(int i = 0; i<delta1.length; i++) {
+				delta1[i]= error1[i] * (sigmoid1d(intermediateAnswer1,true)[i]);
+			}
+			error0 = rotateMultiply(synapticWeights1,delta1);
+			for(int i = 0; i<delta0.length; i++) {
+				delta0[i]= error0[i] * (sigmoid1d(intermediateAnswer0,true)[i]);
+			}
+			for(int i=0; i<synapticWeights2.length; i++) {
+				for(int j=0; j<synapticWeights2[0].length; j++) {
+					synapticWeights2[i][j] +=  rotateMultiply(delta2 , intermediateAnswer1)[i][j];
 				}
 			}
-			System.out.println();
-			for(int i=0; i<dataTaylored.get(k).length; i++) {
-				inputs[i]=dataTaylored.get(k)[i];
+
+			for (int i = 0; i<synapticWeights1.length; i++) {
+				for(int j = 0; j<synapticWeights1[0].length; j++) {
+					double[][] debugger = rotateMultiply(delta1, intermediateAnswer0);
+					synapticWeights1[i][j] += debugger[i][j];
+				}
 			}
-			for(int i=0; i<ans.get(k).length; i++) {
-				desiredOutcome[i] = ans.get(k)[i];
+			for(int i=0; i<synapticWeights0.length; i++) {
+				for(int j=0; j<synapticWeights0[0].length; j++) {
+					synapticWeights0[i][j] +=  rotateMultiply(delta0 , inputs)[i][j];
+				}
 			}
 			long stop = System.currentTimeMillis();
-			double elapsed = (stop - start) / 1000.0;
-			times[k]=elapsed;
+			elapsed = (stop - start) / 1000.0;
 			//System.out.println(m);
-			System.out.println("Time:" + elapsed);
-			//inputs
-		}
-		long outermostStop = System.currentTimeMillis();
-		double outermostElapsed = (outermostStop-outermostStart)/1000.0;
-		//System.out.println();
-		double total = 0;
-		for(int i=0; i<times.length; i++) {
-			total += times[i];
-		}
-		//System.out.println("Synaptic Weights0:");
-		//System.out.println();
-		for(int i=0; i<synapticWeights0.length; i++) {
-			for(double j: synapticWeights0[0]) {
-		//		System.out.println(j);
+			totalTime+=elapsed;
+			//System.out.println("Average time per iteration: "+ (elapsed));
+			double largest = 0;
+			int index = 0;
+			int index2 = 0;
+			int index3 = 0;
+			double second = 0;
+			double third = 0;
+			for(int i = 0; i < finalAnsArr.length; i++) {
+				if(finalAnsArr[i]>largest) {
+					largest = finalAnsArr[i];
+					index = i;
+				}
+			}
+			for(int i = 0; i<finalAnsArr.length; i++) {
+				if(finalAnsArr[i]>second && finalAnsArr[i]<largest) {
+					second = finalAnsArr[i];
+					index2 = i;
+				}
+			}
+			for(int i = 0; i<finalAnsArr.length; i++) {
+				if(finalAnsArr[i]>third && finalAnsArr[i]<second) {
+					third = finalAnsArr[i];
+					index3 = i;
+				}
+			}
+			if(desiredOutcome[index]==1 || desiredOutcome[index2]==1 /*|| desiredOutcome[index3]==1*/) {
+				if(correct.size() == 200) {
+					correct.remove(0);
+				}
+				correct.add(1);
+			}
+			else {
+				if(correct.size() == 200) {
+					correct.remove(0);
+				}
+				correct.add(0);
+			}
+			if(m%200 == 0) {
+				int right = 0;
+				for(double i:correct) {
+					if(i == 1) {
+						right++;
+					}
+				}
+				System.out.println("Last 200 correct:" + right);
+			}
+			if(m%1000 == 0) {
+				double  avg = 0;
+				for(double i: error2) {
+					avg+=i;
+				}
+				avg= avg/error2.length;
+				System.out.println("error " + m + " :" + avg);
+
 			}
 		}
-		//System.out.println();
-		//System.out.println("Synaptic Weights1:");
-		for(int i=0; i<synapticWeights1.length; i++) {
-			for(double j: synapticWeights1[0]) {
-		//		System.out.println(j);
-			}
-		}
-		System.out.println("Average time per outer loop iteration: "+ (total/times.length));
-		System.out.println("Total time for outer and inner for loop iterations: "+ outermostElapsed);
-		//String file1 = "/Users/ethansong/Documents/Matrix Saves/synapticWeights0.csv";
-		//String file2 = "/Users/ethansong/Documents/Matrix Saves/synapticWeights1.csv";
-		//writeCsv(synapticWeights0, file1);
-		//writeCsv(synapticWeights1, file2);
+		System.out.println("Total Time:" + totalTime);
+		/*for(double i: inputs){
+			System.out.println(i);
+		}*/
+
+
 	}
 }
