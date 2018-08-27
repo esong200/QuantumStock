6c098cbc77c52c9cbadf0a8138502fcccdbed704
whoisis <readatontim@gmail.com>
2018-08-26 19:26:27 -0700

    breaks at 8th iteration, should be same as rnn_sample in logic

--- Neural Network Test/rnn tests/testTranslate.java
+++ Neural Network Test/rnn tests/testTranslate.java
@@ -1,123 +1,157 @@
 import java.util.ArrayList;
+import java.util.Random;
 
-public class testTranslation extends CSVReadWrite {
+public class exactTranslation {
 
 	public static void main(String[] args) {
-//	int binary_dim = 8;
-//	int[] int2binary = new int[binary_dim];
-//	int largest_number = (int) Math.pow(2, binary_dim);
-	double alpha = 0.1;
-	String comp = "EA";
-	ArrayList<double[]> data = readCsv("C:\\Users\\Tim Huang\\Documents\\GitHub\\highlighter\\Neural Network Test\\Data\\"+ comp + "DataAdjst.csv");
-	ArrayList<double[]> ans = readCsv("C:\\Users\\Tim Huang\\Documents\\GitHub\\highlighter\\Neural Network Test\\Data\\"+ comp + "Ans.csv");
-	ArrayList<double[]> dataTaylored = data;
-//	ArrayList<Integer> correct = new ArrayList<Integer>();
-	double[][] dataTayloredMatrix = new double[dataTaylored.size()][dataTaylored.get(0).length];
-	int maxSize = data.size();
-	while(ans.size()>maxSize){
-		ans.remove(ans.size()-1);
-	}
-	double[] inputs1 = new double[data.get(0).length];
-	double[] inputs2 = new double[data.get(0).length];
-	double[] desiredOutcome = new double[ans.get(0).length];
-	for(int i=0; i<data.size(); i++) {
-		for(int j=0; j<data.get(i).length; j++) {
-			dataTaylored.get(i)[j] *= 10;
-			dataTayloredMatrix[i][j]=dataTaylored.get(i)[j];
+		Random generator = new Random(2);
+		ArrayList<int[]> binary = new ArrayList<int[]>();
+		int binary_dim = 8;
+
+		int largest_num = (int) Math.pow(2, binary_dim);
+		for(int i = 0; i < largest_num; i++) {
+			binary.add(toBin(i));
+		}
+
+		double alpha = .1;
+		int input_dim = 2;
+		int hidden_dim = 16;
+		int output_dim = 1;
+
+		double[][] synapse_0 = new double[input_dim][hidden_dim]
+				, synapse_0_update = new double[input_dim][hidden_dim]
+				, synapse_1 = new double[hidden_dim][output_dim]
+				, synapse_1_update = new double[hidden_dim][output_dim]
+				, synapse_h = new double[hidden_dim][hidden_dim]
+				, synapse_h_update = new double[hidden_dim][hidden_dim];
+		rand(synapse_0);
+		rand(synapse_1);
+		rand(synapse_h);
+
+		for(int j = 0; j<10000; j++)
+		{
+			int a_int = (int) (generator.nextDouble()*(largest_num/2));
+			int[] a = binary.get(a_int);
+
+			//System.out.println("a: " + a_int);
+
+			int b_int = (int) (generator.nextDouble()*(largest_num/2));
+			int[] b = binary.get(b_int);
+
+			//System.out.println("b: " + b_int);
+			//System.out.println("b from bin: " + fromBin(b));
+
+
+			int c_int = a_int + b_int;
+			int[] c = binary.get(c_int);
+
+			int[] d = binary.get(0);
+
+			double overallError = 0;
+
+			ArrayList<double[]> layer_2_deltas = new ArrayList<double[]>();
+			ArrayList<double[]> layer_1_values = new ArrayList<double[]>();
+			layer_1_values.add(new double[hidden_dim]);
+
+			if(j==8) {
+				System.out.println();
+			}
+
+			for(int position = 0; position<binary_dim; position++) {
+
+				double[] X = {a[binary_dim - position -1], b[binary_dim - position -1]};
+				double[] y = {c[binary_dim - position -1]};
+
+				double[] layer_1 = functions.sigmoid1d(functions.add(functions.dotMultiply(X, synapse_0),
+						functions.dotMultiply(layer_1_values.get(layer_1_values.size()-1), synapse_h)), false);
+
+				double[] layer_2 = functions.sigmoid1d(functions.dotMultiply(layer_1, synapse_1), false);
+
+				double[] layer_2_error = functions.subtract(y, layer_2);
+				layer_2_deltas.add(functions.multiply(layer_2_error, functions.sigmoid1d(layer_2, true)));
+				overallError += Math.abs(layer_2_error[0]);
+
+				d[binary_dim-position-1] = (int) Math.round(layer_2[0]);
+
+				layer_1_values.add(layer_1.clone());
+			}
+
+			double[] future_layer_1_delta = new double[hidden_dim];
+
+			for(int position = 0; position < binary_dim; position++) {
+
+				double[] X = {a[position], b[position]};
+				double[] layer_1 = layer_1_values.get(layer_1_values.size()-position-1);
+				double[] prev_layer_1 = layer_1_values.get(layer_1_values.size()-position-2);
+
+				double[] layer_2_delta = layer_2_deltas.get(layer_2_deltas.size()-position-1);
+
+				double[] layer_1_delta = functions.multiply((functions.add(functions.rotateMultiply(future_layer_1_delta, synapse_h),
+						functions.rotateMultiply(layer_2_delta, synapse_1))),functions.sigmoid1d(layer_1, false));
+
+				double[][] intermed = functions.add(synapse_1_update, functions.rotateMultiply(layer_2_delta, layer_1));
+				synapse_1_update = intermed.clone();
+				intermed = functions.add(synapse_h_update, functions.rotateMultiply(layer_1_delta, prev_layer_1));
+				synapse_h_update = intermed.clone();
+				intermed = functions.add(synapse_0, functions.rotateMultiply(layer_1_delta, X));
+				synapse_0_update = intermed.clone();
+
+				future_layer_1_delta = layer_1_delta.clone();
+			}
+			double[][] intermed;
+			intermed = functions.add(synapse_0, functions.multiply(synapse_0_update, alpha));
+			synapse_0 = intermed.clone();
+			intermed = functions.add(synapse_1, functions.multiply(synapse_1_update, alpha));
+			synapse_1 = intermed.clone();
+			intermed = functions.add(synapse_h, functions.multiply(synapse_h_update, alpha));
+			synapse_h = intermed.clone();
+
+			intermed = functions.multiply(synapse_0_update, 0);
+			synapse_0_update = intermed.clone();
+			intermed = functions.multiply(synapse_1_update, 0);
+			synapse_1_update = intermed.clone();
+			intermed = functions.multiply(synapse_h_update, 0);
+			synapse_h_update = intermed.clone();
+
+			//if(j%1000 == 0) {
+				System.out.println("trail: " + j);
+				System.out.println("Error: " + overallError);
+				System.out.println("True: " + fromBin(c));
+				System.out.println("Pred: " + fromBin(d));
+			//}
 		}
 	}
 
-	double[][] synap0 = new double[inputs1.length][12];
-	double[][] synap1 = new double[synap0[0].length][8];
-	double[][] synapH = new double[synap0[0].length][synap0[0].length];
-
-
-
-	double[] layer1 = new double[synap0[0].length];
-	double[] layer2 = new double[synap1[0].length];
-
-	ArrayList<double[]> layer1Values = new ArrayList<double[]>();
-	ArrayList<double[]> layer2Values = new ArrayList<double[]>();
-	layer1Values.add(new double[layer1.length]);
-	layer2Values.add(new double[layer2.length]);
-
-	double[] error2 = new double[layer2.length];
-	ArrayList<double[]> deltas2 = new ArrayList<double[]>();
-
-	double[] delta1 = new double[synap0[0].length];
-	double[] delta2 = new double[synap1[0].length];
-
-	for(int m = 0; m<10000; m++) {
-		int rand = (int) ((int) (dataTaylored.size()-1)*Math.random());
-		System.out.println(rand);
-		inputs1 = dataTaylored.get(rand).clone();
-		inputs2 = dataTaylored.get(rand + 1);
-
-		for(int i = dataTaylored.size()-1; i>-1; i--) {
-			inputs1 = dataTaylored.get(i);
-			desiredOutcome = ans.get(i);
-			for(int j =0; j<layer1.length; j++) {
-				layer1[j] = functions.sigmoid1d(functions.dotMultiply(inputs1,synap0), false)[j]
-						+functions.dotMultiply(layer1Values.get(layer1Values.size()-1), synapH)[j];
-			}
-			layer2 = functions.sigmoid1d(functions.dotMultiply(layer1, synap1), false);
-			for(int j = 0; j<error2.length; j++) {
-				error2[j] = desiredOutcome[j] - layer2[j];
-				delta2[j] = error2[j] * functions.sigmoid1d(layer2, true)[j];
-			}
-			deltas2.add(delta2.clone());
-			layer1Values.add(layer1.clone());
+	private static int[] toBin(int nonBin) {
+		int size = 9;
+/*		while(Math.pow(2, size)<= nonBin) {
+			size++;
+		}*/
+		int[] binary = new int[size];
+		for(int i = size-1; i >= 0; i--) {
+			binary[i] = nonBin%2;
+			nonBin = nonBin/2;
 		}
-		double[] delta1Future = new double[delta1.length];
-		double[][] synap0Up = new double[inputs1.length][12];
-		double[][] synap1Up = new double[synap0[0].length][8];
-		double[][] synapHUp = new double[synap0[0].length][synap0[0].length];
-		for(int i = 0; i<dataTaylored.size(); i++) {
-			inputs2 = dataTaylored.get(i);
-			layer1 = layer1Values.get(layer1Values.size()-i-1);
-			double[] layer1Pre = layer1Values.get(layer1Values.size()-2-i);
-
-			delta2 = deltas2.get(deltas2.size()-i-1);
-			for(int j = 0; j<delta1.length; j++) {
-				delta1[j] = (functions.dotMultiply(delta1Future, synapH)[j]
-						+ functions.rotateMultiply(delta2, synap1)[j])
-						*functions.sigmoid1d(layer1, true)[j];
-			}
-			for(int j = 0; j <synap1Up.length; j++) {
-				for(int k = 0; k<synap1Up[0].length; k++) {
-					synap1Up[j][k] += functions.rotateMultiply(delta2, layer1)[j][k];
-				}
-			}
-			for(int j = 0; j <synapHUp.length; j++) {
-				for(int k = 0; k<synapHUp[0].length; k++) {
-					synapHUp[j][k] += functions.rotateMultiply(delta1, layer1Pre)[j][k];
-				}
-			}
-			for(int j = 0; j <synap0Up.length; j++) {
-				for(int k = 0; k<synap0Up[0].length; k++) {
-					synap0Up[j][k] += functions.rotateMultiply(delta1, inputs2)[j][k];
-				}
-			}
-			delta1Future = delta1.clone();
-		}
-		for(int i= 0; i< synap0.length; i++) {
-			for(int j=0; j<synap0[0].length; j++) {
-				synap0[i][j] += synap0Up[i][j]*alpha;
-			}
-		}
-		for(int i= 0; i< synap1.length; i++) {
-			for(int j=0; j<synap1[0].length; j++) {
-				synap1[i][j] += synap1Up[i][j]*alpha;
-			}
-		}
-		for(int i= 0; i< synapH.length; i++) {
-			for(int j=0; j<synapH[0].length; j++) {
-				synapH[i][j] += synapHUp[i][j]*alpha;
-			}
-		}
-
-		}
+		return binary;
 	}
 
+	private static int fromBin(int[] Bin) {
+		int returnInt = 0;
+		for(int i = 0; i<Bin.length; i++) {
+			if(Bin[Bin.length-i-1] == 1) {
+				returnInt+=(int) Math.pow(2, i);
+			}
+		}
+		return returnInt;
+	}
+
+	private static void rand(double[][] rand) {
+		Random generator = new Random(2);
+		for (int i = 0; i< rand.length; i++) {
+			for(int j = 0 ;j <rand[0].length; j++) {
+				rand[i][j] = generator.nextDouble()*2 -1;
+			}
+		}
+	}
 
 }
