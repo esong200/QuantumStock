021ebbd76f11930269e650a7023aff53eeb8a34e
whoisis <readatontim@gmail.com>
2018-08-28 14:53:03 -0700

    redid w/ dagaadjust and large data add way to update data

--- Neural Network Test/src/DataWriting.java
+++ Neural Network Test/src/DataWriting.java
@@ -14,50 +14,72 @@ public class DataWriting extends AlphaVantageCollector {
 //		writeCsv(ansr,filePaths);
 		//monthlyData("AAPL");
 		String[] comps = {  "ARLP", "ATVI", "CTL", "DE", "EA", "FTI", "HAS", "IVZ", "KSS", "MO", "NVDA", "PDLI", "PEP", "STT", "T", "WYNN"};
-
+//for new comps
 		for(String comp:comps) {
+			writeData(comp);
+		}
+//For first of month's to gather last month data
+		for(String comp: comps) {
+			addData(comp);
+		}
 
+	}
 
-		//String filePathD = "C:\\Users\\Tim Huang\\eclipse\\StockProofReading\\" + comp + "Data.csv";
-			String filePathA = "C:\\Users\\Tim Huang\\Documents\\GitHub\\highlighter\\Neural Network Test\\Data\\" + comp + "Ans.csv";
-			ArrayList<double[]> ANDVDat = DataCollection(comp);
-			try {
-				System.out.println("sleeping");
-				Thread.sleep(60000);
+	private static void writeData(String comp) {
+		String filePathA = "C:\\Users\\Tim Huang\\Documents\\GitHub\\highlighter\\Neural Network Test\\ShortTimeBigData\\" + comp + "Ans.csv";
+		ArrayList<double[]> ANDVDat = DataCollection(comp);
+		try {
+			System.out.println("sleeping");
+			Thread.sleep(60000);
 
-			} catch (InterruptedException e1) {
-				e1.printStackTrace();
-			}
-			ArrayList<double[]> ANDVAns = answerCompile(comp);
-			ArrayList<double[]> adjst = new ArrayList<double[]>();
-			for(int j = 0; j<ANDVDat.size()-1; j++) {
-				double[] current = ANDVDat.get(j);
-				double[] previous = ANDVDat.get(j+1);
-				double[] perc = new double[current.length];
-				for(int k = 0; k<current.length; k++) {
-					perc[k] = (current[k]-previous[k])/previous[k];
-					if(previous[k] == 0) {
-						perc[k] = 0;
-					}
+		} catch (InterruptedException e1) {
+			e1.printStackTrace();
+		}
+		ArrayList<double[]> ANDVAns = answerCompile(comp);
+		ArrayList<double[]> adjst = new ArrayList<double[]>();
+		for(int j = 0; j<ANDVDat.size()-1; j++) {
+			double[] current = ANDVDat.get(j);
+			double[] previous = ANDVDat.get(j+1);
+			double[] perc = new double[current.length];
+			for(int k = 0; k<current.length; k++) {
+				perc[k] = (current[k]-previous[k])/previous[k];
+				if(previous[k] == 0) {
+					perc[k] = 0;
 				}
-				adjst.add(perc);
 			}
-			String newFile = "C:\\Users\\Tim Huang\\Documents\\GitHub\\highlighter\\Neural Network Test\\Data\\" + comp + "DataAdjst.csv";
+			adjst.add(perc);
+		}
+		String newFile = "C:\\Users\\Tim Huang\\Documents\\GitHub\\highlighter\\Neural Network Test\\ShortTimeBigData\\" + comp + "BigDataAdjst.csv";
 
-			while(ANDVAns.size()>adjst.size()) {
-				ANDVAns.remove(ANDVAns.size()-1);
-			}
-			writeCsv(adjst, newFile);
-			//writeCsv(ANDVDat, filePathD);
-			writeCsv(ANDVAns, filePathA);
-			try {
-				System.out.println("sleeping");
-				Thread.sleep(60000);
+		while(ANDVAns.size()>adjst.size()) {
+			ANDVAns.remove(ANDVAns.size()-1);
+		}
+		writeCsv(adjst, newFile);
+		//writeCsv(ANDVDat, filePathD);
+		writeCsv(ANDVAns, filePathA);
+		try {
+			System.out.println("sleeping");
+			Thread.sleep(60000);
 
-			} catch (InterruptedException e1) {
-				e1.printStackTrace();
-			}
+		} catch (InterruptedException e1) {
+			e1.printStackTrace();
 		}
 	}
 
+	private static void addData(String comp) {
+		String dataPath = "C:\\Users\\Tim Huang\\Documents\\GitHub\\highlighter\\Neural Network Test\\ShortTimeBigData\\" + comp + "BigDataAdjst.csv";
+		String ansPath = "C:\\Users\\Tim Huang\\Documents\\GitHub\\highlighter\\Neural Network Test\\ShortTimeBigData\\" + comp + "Ans.csv";
+
+		ArrayList<double[]> data = readCsv(dataPath);
+		ArrayList<double[]> ans = readCsv(ansPath);
+
+		double[] addData = DataAdd(comp);
+		double[] addAns = addAns(comp);
+
+		data.add(0, addData);
+		ans.add(0, addAns);
+
+		writeCsv(data, dataPath);
+		writeCsv(ans, ansPath);
 	}
+}
